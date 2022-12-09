338b5106924395addae618597b7a609f092c5761
watarunakade <wataru092783@gmail.com>
2022-12-09 14:43:35 +0900

    usersテーブルの作成

--- db/migrate/20221209052427_devise_create_users.rb
+++ db/migrate/20221209052427_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false #「nameカラム」は空の値を許可しないNOTNULL制約で作成される
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
