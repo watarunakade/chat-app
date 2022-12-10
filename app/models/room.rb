fed6575901e08c9c096488bcebf43325002e9db6
watarunakade <wataru092783@gmail.com>
2022-12-10 21:15:14 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many :room_users
+  has_many :users, through: :room_users # room_usersを経由してusers.rbと関係を持っている。
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   #nameカラムに、presence:trueを設けることで、空の場合はDBに保存しないというバリデーションを設定する。
+  
+  has_many :room_users
+  has_many :rooms, through: :room_users # room_usersを通してroom.rbと関係を持っている。
 end
