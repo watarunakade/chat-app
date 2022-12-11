75c2fe8c3c90387ad5711d4f8d73c93f7f92d924
watarunakade <wataru092783@gmail.com>
2022-12-12 00:05:04 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users # room_usersを経由してusers.rbと関係を持っている。
-  has_many :mesages
+  has_many :messages, dependent: :destroy
   validates :name, presence: true
 
 end
