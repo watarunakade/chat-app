016ba6b4be12191c8bbf41077bb12031df31d3ae
watarunakade <wataru092783@gmail.com>
2022-12-11 23:04:12 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])#paramsに含まれているroom_idを代入する
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save #メッセージを保存できた時
+      redirect_to room_messages_path(@room)
+    else #できなかった時
+      render :index
+    end
+  end
+
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
